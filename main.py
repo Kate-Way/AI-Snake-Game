efa7be3bed903f1ed582654cb6a4ba2fd3f2c2e6
Kate Wayenberg <106112580+Kate-Way@users.noreply.github.com>
2022-09-28 15:59:54 -0700

    Changed color

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pygame
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor('black')
+screen.bgcolor('#9CAF88')
 screen.title('Snake Game')
 screen.tracer(0)
 clock = pygame.time.Clock()
